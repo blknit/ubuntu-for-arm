97ba58483ee4622e59b482d3b5e61c88afaf80c0
Tongqing Liu <tongqing.liu@opswat.com>
2023-06-25 23:57:21 -0700

    add orangepi-h616 kernel and uboot package

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -eE 
 trap 'echo Error: in $0 on line $LINENO' ERR
-support_boards=(raspberrypi-armv6 raspberrypi-armv7 raspberrypi-armv8 radxa-zero)
+support_boards=(raspberrypi-armv6 raspberrypi-armv7 raspberrypi-armv8 radxa-zero mangopi-h616)
 
 usage() {
 cat << HELPDOC
